7802d0cfb8fac508c56b4ac11ab4ab5557ecd286
Fernando Maia <2966576+fernandomaia@users.noreply.github.com>
2022-11-18 17:46:22 -0300

    add function to checkout to GitHub's PR

--- zsh/.config/zsh/git.zsh
+++ zsh/.config/zsh/git.zsh
@@ -16,3 +16,37 @@ del_branches() {
     git branch -D $branches_to_delete
   fi
 }
+
+pr_checkout() {
+  local jq_template pr_number
+
+  jq_template='"'\
+'#\(.number) - \(.title)'\
+'\t'\
+'Author: \(.user.login)\n'\
+'Created: \(.created_at)\n'\
+'Updated: \(.updated_at)\n\n'\
+'\(.body)'\
+'"'
+
+  if [[ ! -z "${1}" ]]
+  then
+    pr_number=${1}
+  else
+    pr_number=$(
+      gh api 'repos/:owner/:repo/pulls' |
+      jq ".[] | $jq_template" |
+      sed -e 's/"\(.*\)"/\1/' -e 's/\\t/\t/' |
+      fzf \
+        --with-nth=1 \
+        --delimiter='\t' \
+        --preview='echo -e {2}' \
+        --preview-window=top:wrap |
+      sed -e 's/ .*//' -e 's/^.//'
+    )
+  fi
+
+  if [ -n "$pr_number" ]; then
+    gh pr checkout $pr_number
+  fi
+}
