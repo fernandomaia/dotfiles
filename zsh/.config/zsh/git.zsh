eccf566a9883bae87bc9341a4c2d10740becc05f
Fernando Maia <2966576+fernandomaia@users.noreply.github.com>
2022-11-18 17:39:23 -0300

    add functions to delete and sync branches

--- zsh/.config/zsh/git.zsh
+++ zsh/.config/zsh/git.zsh
@@ -1,1 +1,18 @@
 alias gfc='git branch | fzf | xargs git checkout'
+
+sync_branches() {
+  git remote prune origin
+  for merged_branch in $(git for-each-ref --format '%(refname:short)' --merged HEAD refs/heads/)
+  do
+    git branch -d ${merged_branch}
+  done
+}
+
+del_branches() {
+  local branches_to_delete
+  branches_to_delete=($(git for-each-ref --format '%(refname:short)' --merged HEAD refs/heads/ | fzf --multi))
+
+  if [ -n "$branches_to_delete" ]; then
+    git branch -D $branches_to_delete
+  fi
+}
