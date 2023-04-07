843a8464ff7998cd46d5fc926b2c0e1f142e8658
Fernando Maia <2966576+fernandomaia@users.noreply.github.com>
2023-04-06 21:12:50 -0300

    Add LSP keymaps and Lua setup

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -16,6 +16,25 @@ lsp.ensure_installed({
   'solargraph',
   'tsserver',
   'eslint',
+  'lua_ls',
 })
+
+lsp.configure('lua_ls', {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+end)
 
 lsp.setup()
