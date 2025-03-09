e203c4d94c35f6297522eb73f53e535548677238
Fernando Maia <2966576+fernandomaia@users.noreply.github.com>
2025-03-09 18:28:59 -0300

    [nvim] Add avante.nvim

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,29 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- set this if you want to always pull the latest change
+  opts = {
+    -- add any opts here
+    provider = "claude", -- Recommend using Claude
+    behaviour = {
+      auto_suggestions = false, -- Experimental stage
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
