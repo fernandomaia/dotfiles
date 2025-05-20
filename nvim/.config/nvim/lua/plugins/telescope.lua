c46421e55e22959767ac2c37f70ec9ca8dcd9371
Fernando Maia <2966576+fernandomaia@users.noreply.github.com>
2025-05-19 22:36:12 -0300

    [nvim] Update telescope setup to allow searching for hidden files
    Also adds descriptions to Telescope keymaps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -9,10 +9,39 @@ return {
     require('telescope').setup({})
 
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+
+    vim.keymap.set('n', '<leader>pf',
+      function()
+        builtin.find_files({
+          hidden = true,
+          no_ignore = false,
+        })
+      end,
+      { desc = 'Lists files in your current working directory, respects .gitignore' }
+    )
+    vim.keymap.set('n', '<C-p>',
+      function()
+        builtin.git_files()
+      end,
+      { desc = 'Fuzzy search through the output of `git ls-files` command, respects .gitignore' }
+    )
+    vim.keymap.set('n', '<leader>ps',
+      function()
+        builtin.live_grep()
+      end,
+      { desc = 'Search for a string in your current working directory and get results live as you type, respects .gitignore' }
+    )
+    vim.keymap.set('n', '<leader>pb',
+      function()
+        builtin.buffers()
+      end,
+      { desc = 'Lists open buffers' }
+    )
+    vim.keymap.set('n', '<leader>ph',
+      function()
+        builtin.help_tags()
+      end,
+      { desc = 'Lists available help tags and opens a new window with the relevant help info on <cr>' }
+    )
   end
 }
